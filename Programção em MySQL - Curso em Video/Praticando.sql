b4dbd973dcdfba2b2b145697802046ae5aa4c302
felipepatente <felipepatente@yahoo.com.br>
2017-08-02 19:27:30 -0300

    Criando uma trigger

--- Programção em MySQL - Curso em Video/Praticando.sql
+++ Programção em MySQL - Curso em Video/Praticando.sql
@@ -16,3 +16,55 @@ CREATE TABLE estoque(
 );
 
 ALTER TABLE estoque ADD CONSTRAINT fk_Estoque FOREIGN KEY (idProduto) REFERENCES produto (idProduto) ;
+
+SELECT * FROM produto;
+SELECT * FROM estoque;
+
+
+DELIMITER $$
+CREATE TRIGGER tr_InserirQuantidade BEFORE INSERT
+ON produto
+FOR EACH ROW
+
+BEGIN
+	
+	DECLARE @id INT
+	DECLARE @qtdAtual INT
+
+	SELECT @id = idProduto, @qtdAtual = qtdAtual FROM inserted
+
+	INSERT INTO estoque (idProduto, qtdMax) VALUES (@id, @qtdAtual * 2);
+    
+END
+DELIMITER;
+
+
+
+DELIMITER $$
+CREATE TRIGGER antesDeUpdate_empregados 
+BEFORE UPDATE ON empregados
+FOR EACH ROW BEGIN
+INSERT INTO empregados_auditoria
+SET acao = 'update',
+id_empregado = OLD.id_empregado,
+sobrenome = OLD.sobrenome,
+modificadoem = NOW(); END$$
+DELIMITER ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
